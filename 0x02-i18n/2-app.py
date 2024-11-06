5684b28c64a33dbc8d403a32706305548780fa74
omotomiwa26 <omotomiwa26@gmail.com>
2024-11-06 02:54:54 +0100

    2. Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,12 +22,14 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-# @babel.localeselector
 def get_locale() -> str:
     """
         The locale for a web page.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
